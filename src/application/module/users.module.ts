6e751ddcf9e2d4ec69ddffc4cef84a9c5882ed71
dac <93912537+dacdev205@users.noreply.github.com>
2025-01-26 12:45:43 +0700

    feat: add users module

---
+++ src/application/module/users.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from '../controllers/users/users.controller';
+import { UserRepository } from '../repositories/users.repositories';
+import { HashService } from '../services/hash.service';
+import { CreateUserUseCase } from '../use-cases/users/create-user.use-case';
+import { UpdateUserUseCase } from '../use-cases/users/delete-user.use-case';
+import { FindAllUsersUseCase } from '../use-cases/users/find-all-user.use-case';
+import { FindUserByEmailUseCase } from '../use-cases/users/find-user-by-email.use-case';
+import { FindUserByIdUseCase } from '../use-cases/users/find-user-by-id.use-case';
+import { FindUserByPhoneUseCase } from '../use-cases/users/find-user-by-phone.use-case';
+import { DeleteUserUseCase } from '../use-cases/users/update-user.use-case';
+
+@Module({
+  controllers: [UsersController],
+  providers: [
+    UserRepository,
+    HashService,
+    CreateUserUseCase,
+    DeleteUserUseCase,
+    FindAllUsersUseCase,
+    FindUserByPhoneUseCase,
+    UpdateUserUseCase,
+    FindUserByEmailUseCase,
+    FindUserByIdUseCase,
+  ],
+  exports: [
+    UserRepository,
+    HashService,
+    CreateUserUseCase,
+    DeleteUserUseCase,
+    FindAllUsersUseCase,
+    FindUserByPhoneUseCase,
+    UpdateUserUseCase,
+    FindUserByEmailUseCase,
+    FindUserByIdUseCase,
+  ],
+})
+export class UsersModule {}
