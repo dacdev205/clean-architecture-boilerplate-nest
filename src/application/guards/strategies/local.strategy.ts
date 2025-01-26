91225d41715eb20360f65ab49fbc1eb60e7247ef
dac <93912537+dacdev205@users.noreply.github.com>
2025-01-26 19:26:02 +0700

    feat: add local strategy  guard

---
+++ src/application/guards/strategies/local.strategy.ts
@@ -0,0 +1,23 @@
+import { Strategy } from 'passport-local';
+import { UserProfile } from '~/application/responses/user-profile-response';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ValidateUserUseCase } from './../../use-cases/users/validate-user.use-case';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private validateUserUseCase: ValidateUserUseCase) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(email: string, password: string): Promise<UserProfile> {
+    const user = await this.validateUserUseCase.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user as UserProfile;
+  }
+}
