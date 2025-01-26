260303d53605344717e0b77fd06de5c2f5e4f110
dac <93912537+dacdev205@users.noreply.github.com>
2025-01-26 19:30:51 +0700

    feat: add all module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './application/module/auth.module';
+import { BrandModule } from './application/module/brands.module';
+import { CategoryModule } from './application/module/categories.module';
+import { ProductModule } from './application/module/products.module';
+import { UsersModule } from './application/module/users.module';
+import { EmailModule } from './infrastructure/email/email.module';
+import { QueuesModule } from './infrastructure/queues/queues.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST || '127.0.0.1',
+        port: parseInt(process.env.REDIS_PORT) || 6379,
+      },
+    }),
+    CategoryModule,
+    BrandModule,
+    ProductModule,
+    UsersModule,
+    AuthModule,
+    QueuesModule,
+    EmailModule,
+  ],
 })
 export class AppModule {}
