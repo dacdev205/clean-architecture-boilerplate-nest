8375bb3947ebe33b0b2ec7196faabfe3d9a062c8
dac <93912537+dacdev205@users.noreply.github.com>
2025-02-05 16:17:13 +0700

    feat: add env config file

---
+++ src/config/env.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import dotenv from 'dotenv';
+import { DEFAULT_NODE_ENV } from '~/common/constants/envConstants';
+export const readEnvFilePath = (path: string) => {
+  const envVaribles = dotenv.config({ path: path.trim() });
+  return envVaribles;
+};
+
+export const getCurrentEnvFilePath = (): string => {
+  return resolve(
+    process.cwd(),
+    `.env.${process.env.NODE_ENV || DEFAULT_NODE_ENV}`,
+  );
+};
