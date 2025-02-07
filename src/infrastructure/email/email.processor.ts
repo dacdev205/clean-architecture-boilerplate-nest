3eab09f550ff94bd19c37a7323ffc245842b1ff8
dac <93912537+dacdev205@users.noreply.github.com>
2025-02-07 10:20:10 +0700

    feat: implement lazy module for email processor

--- src/infrastructure/email/email.processor.ts
+++ src/infrastructure/email/email.processor.ts
@@ -3,24 +3,28 @@ import { JOB_NAMES, QUEUE_NAMES } from '~/common/constants/queue.constants';
 import { ActivationJobData } from '~/common/interfaces/activation-job-data.interface';
 import { ResetPassJobData } from '~/common/interfaces/reset-pass-job-data.interface';
 import { Processor, WorkerHost } from '@nestjs/bullmq';
-import { EmailService } from './email.service';
+import { LazyModuleLoader } from '@nestjs/core';
 @Processor(QUEUE_NAMES.AUTH_QUEUE)
 export class EmailProcessor extends WorkerHost {
-  constructor(private readonly _emailService: EmailService) {
+  constructor(private readonly _lazyModuleLoader: LazyModuleLoader) {
     super();
   }
   async process(job: Job<any>): Promise<void> {
+    const { EmailModule } = await import('../email/email.module');
+    const moduleRef = this._lazyModuleLoader.load(() => EmailModule);
+    const { EmailService } = await import('./email.service');
+    const emailJobService = (await moduleRef).get(EmailService, {
+      strict: false,
+    });
     switch (job.name) {
       case JOB_NAMES.SEND_ACTIVATION_MAIL: {
-        await this._emailService.sendActivationEmail(
+        await emailJobService.sendActivationEmail(
           job.data as ActivationJobData,
         );
         break;
       }
       case JOB_NAMES.SEND_RESET_PASSWORD_MAIL: {
-        await this._emailService.sendResetPassEmail(
-          job.data as ResetPassJobData,
-        );
+        await emailJobService.sendResetPassEmail(job.data as ResetPassJobData);
         break;
       }
     }
